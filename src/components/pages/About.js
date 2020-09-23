17a2baaad942a9241c0c682c334614cff3747fbb
AhsanKhan7 <innocent.ahsan12345@gmail.com>
2020-09-22 23:00:29 -0700

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+                <h1>About</h1>
+                <h3>This is todoList App version1.0.0 this is part of React-Crash-Course</h3>
+        </React.Fragment>
+    )
+}
+
+export default About
